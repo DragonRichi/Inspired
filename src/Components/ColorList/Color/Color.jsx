f1686d2166dc40e601d886250c3c8051003b10bd
DragonRichi <lucasrichi123@gmail.com>
2023-07-07 12:22:13 +0300

    4

--- src/Components/ColorList/Color/Color.jsx
+++ src/Components/ColorList/Color/Color.jsx
@@ -2,12 +2,11 @@ import { useRef } from 'react';
 import styles from './Color.module.scss'
 export const Color = ({ color, check }) => {
     const ref = useRef(color)
-    const t = ref.current;
-    console.log(t)
+
     console.log(ref)
     return (
-        <li className={styles.color}>
-            {t}
+        <li className={styles.color} ref={ref}>
+            {check}
         </li>
     );
 }
